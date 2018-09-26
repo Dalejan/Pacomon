764d031cb385e18516fe5669ad7fc8d793c1412c
mateo2708 <mateotafurg@gmail.com>
2018-09-26 09:29:51 -0500

    Se crea la directiva life-points para asignar el color y el tamaño de la barra de vida

---
+++ src/directives/life-points.directive.spec.ts
@@ -0,0 +1,8 @@
+import { LifePointsDirective } from './life-points.directive';
+
+describe('LifePointsDirective', () => {
+  it('should create an instance', () => {
+    const directive = new LifePointsDirective();
+    expect(directive).toBeTruthy();
+  });
+});
