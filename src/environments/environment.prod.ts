322921bdb12658c1cb9cb7c53b74efba921c71ae
Zestian <zestian56@gmail.com>
2018-10-08 19:17:49 -0500

    añadido responsive, y service worker para implementación de pwa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDGF9Dzr4v5Boq0uh6EDd6eNOYRw5pxrgI',
+    authDomain: 'pacomon-3fb61.firebaseapp.com',
+    databaseURL: 'https://pacomon-3fb61.firebaseio.com',
+    projectId: 'pacomon-3fb61',
+    storageBucket: 'pacomon-3fb61.appspot.com',
+    messagingSenderId: '27916133c8538'
+  }
 };
